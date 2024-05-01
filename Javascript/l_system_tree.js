a9f3509674bba8cda394865fdf5f59b67b282795
osman <osman>
2024-05-01 11:42:58 +0200

    l_system

---
+++ Javascript/l_system_tree.js
@@ -0,0 +1,109 @@
+/*
+
+axiom = F
+F -> FF+[+F-F-F]-[-F+F+F]
+angle = 22.5
+
+*/
+
+var angle;
+var axiom = "F"
+var sentence = axiom
+var len = 100
+
+var rules = []
+rules[0] = {
+  a : "F",
+  b : "FF+[+F-F-F]-[-F+F+F]"
+}
+  
+function generate(){
+  len *= 0.5
+  var nextSentence = ""
+  for(var i=0; i<sentence.lenght; i++){
+    var current = sentence.charAt(i)
+    var found = false
+    for(var j=0; j<rules.length; j++){
+      if(current == rules[j].a){
+        found = true
+        nextSentence += rules[j].b
+        break
+      }
+    }
+      if(!found){
+        nextSentence += current
+      }
+  }
+    sentence = nextSentence
+    createP(sentence)
+    turtle()
+}
+  
+
+function turtle(){
+  background(51)
+  resetMatrix()
+  translate(width / 2, height)
+  stroke(255, 100)
+  for(var i=0; i<sentence.length; i++){
+    var current = sentence.charAt(i)
+    
+    /*
+    
+    switch (current){
+      case "F":
+        line(0,0,0,-len)
+        translate(0,-len)
+        break
+      case "+":
+        rotate(angle)
+        break
+      case "-":
+        rotate(-angle)
+        break
+      case "[":
+        push()
+        break
+      case "]":
+        pop()
+        break
+    }
+    
+    */
+    
+    if (current == "F") {
+      line(0, 0, 0, -len);
+      translate(0, -len);
+    } else if (current == "+") {
+      rotate(angle);
+    } else if (current == "-") {
+      rotate(-angle)
+    } else if (current == "[") {
+      push();
+    } else if (current == "]") {
+      pop();
+    }
+    
+  }
+}
+
+
+function setup() {
+  createCanvas(400, 400);
+  angle = radians(22.5)
+  background(51);
+  createP(axiom)
+  turtle()
+  var button = createButton("generate")
+  button.mousePressed(generate)
+}
+ 
+/*
+
+function mousePressed(){
+  generate()
+  //turtle()
+}
+
+*/
+
