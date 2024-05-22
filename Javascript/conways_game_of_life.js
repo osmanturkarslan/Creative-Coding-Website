ffa6f0aec1ee855490176a91aacad32afe15f4e4
osman <osman>
2024-05-22 11:45:29 +0200

    nw

--- Javascript/conways_game_of_life.js
+++ Javascript/conways_game_of_life.js
@@ -1,148 +1,194 @@
-const BOARD_SQUARE_ARR = [];
+const BOARD_SQUARE_ARR_2D = [];
+const SQUARE_ARR_2D =[];
 const BOARD_SQUARE_SIZE = 30;
-const SQUARE_ARR = [];
-let alive = true;
-let filled = true;
-let numOfSquaresInXDirection = 0;
-let numOfSquaresInYDirection = 0;
-
-let arr2D = [];
-
-// console.log(arr2D[0][1])
+let alive = false;
+// let numOfSquaresInXDirection = 0;
+// let numOfSquaresInYDirection = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(225);
 
   createSquareBoard();
+  
 
-  //! Output number of Square
-  console.log(
-    "x: " + numOfSquaresInXDirection + ", y: " + numOfSquaresInYDirection
-  );
-  // Square.starterSquare()
-  // mousePressed();
-
+  // // ! Output number of board square
+  // console.log(
+  //   "x: " + numOfSquaresInXDirection + ", y: " + numOfSquaresInYDirection
+  // );
   // ! Debug Only
-  //console.log(BOARD_SQUARE_ARR);
-  //print(BOARD_SQUARE_ARR[34]);
-  // console.log(BOARD_SQUARE_ARR);
-  console.log(arr2D);
-  // print(SQUARE_ARR[3]);
-  // ! Debug Only
-
-  //let randomStart = int(random(SQUARE_ARR.length))
-  //console.log(MySquare.starterSquare().randomStart)
-  //console.log(SQUARE_ARR[randomStart])
-
-  //SQUARE_ARR[randomStart].alive ? fill(255) : fill(0);
-  //square(SQUARE_ARR[randomStart].x, SQUARE_ARR[randomStart].y, SQUARE_SIZE);
+  console.log(BOARD_SQUARE_ARR_2D);
+  console.log(SQUARE_ARR_2D)
 }
 
-// function draw(){
-//   for(let x=1; x<rows.length-1; x++){
-//     for(let y=1; y<columns.length-1; y++){
-
-//   }
-//   }
-// }
+function draw(){
+  checkNeighbourAlive();
+  arrayAssingment()
+  cellPaint()
+}
 
 function createSquareBoard() {
   for (let x = 1; x < windowWidth - 1; x += BOARD_SQUARE_SIZE + 1) {
-    arr2D.push([]);
-    numOfSquaresInXDirection++;
+    BOARD_SQUARE_ARR_2D.push([]);
+    SQUARE_ARR_2D.push([]);
+    // numOfSquaresInXDirection++;
     for (let y = 1; y < windowHeight - 1; y += BOARD_SQUARE_SIZE + 1) {
-      if (x == 1) {
-        numOfSquaresInYDirection++;
-      }
+      // if (x == 1) {
+      //   numOfSquaresInYDirection++;
+      // }
       fill(225);
       stroke(120);
       square(x, y, BOARD_SQUARE_SIZE);
-      const squareCoordinate = { x, y, filled, alive };
-      // SQUARE_ARR.push(new Square(x, y, alive));    //Square classdaki constructor ile calisir.
-      BOARD_SQUARE_ARR.push(squareCoordinate);
-      arr2D[arr2D.length - 1].push(squareCoordinate);
+      const squareCoordinate = { x, y, alive };
+      BOARD_SQUARE_ARR_2D[BOARD_SQUARE_ARR_2D.length - 1].push(squareCoordinate);
+      SQUARE_ARR_2D[SQUARE_ARR_2D.length - 1].push(squareCoordinate);
     }
   }
 }
 
 function mousePressed() {
-  // BOARD_SQUARE_ARR[randomStart].alive ? fill(180, 28, 28) : fill(0);
-
-  for (let x = 0; x < arr2D.length; x++) {
-    for (let y = 0; y < arr2D[x].length; y++) {
+  for (let x = 0; x < BOARD_SQUARE_ARR_2D.length; x++) {
+    for (let y = 0; y < BOARD_SQUARE_ARR_2D[x].length; y++) {
       let xP = null;
       let yP = null;
       if (
-        mouseX > arr2D[x][y].x &&
-        mouseX < arr2D[x][y].x + BOARD_SQUARE_SIZE
+        mouseX > BOARD_SQUARE_ARR_2D[x][y].x && mouseX < BOARD_SQUARE_ARR_2D[x][y].x + BOARD_SQUARE_SIZE
       ) {
-        xP = arr2D[x][y].x;
+        xP = BOARD_SQUARE_ARR_2D[x][y].x;
       }
       if (
-        mouseY > arr2D[x][y].y &&
-        mouseY < arr2D[x][y].y + BOARD_SQUARE_SIZE
+        mouseY > BOARD_SQUARE_ARR_2D[x][y].y && mouseY < BOARD_SQUARE_ARR_2D[x][y].y + BOARD_SQUARE_SIZE
       ) {
-        yP = arr2D[x][y].y;
+        yP = BOARD_SQUARE_ARR_2D[x][y].y;
       }
 
       fill(180, 28, 28);
-      // square(x, y, BOARD_SQUARE_SIZE);
       if (xP != null && yP != null) {
         square(xP, yP, BOARD_SQUARE_SIZE);
-        const squareCoordinate = { xP, yP, filled, alive };
-
-        SQUARE_ARR.push(squareCoordinate);
-        // console.log(SQUARE_ARR);
-
-        return;
+        BOARD_SQUARE_ARR_2D[x][y].alive = true;
       }
     }
   }
-  // for (let i = 0; i < BOARD_SQUARE_ARR.length; i++) {
-  //   let x = null;
-  //   let y = null;
-  //   // if (mouseX > BOARD_SQUARE_ARR[i].x && mouseX < BOARD_SQUARE_ARR[i + numOfSquaresInYDirection].x) {
-  //   if (
-  //     mouseX > BOARD_SQUARE_ARR[i].x &&
-  //     mouseX < BOARD_SQUARE_ARR[i + numOfSquaresInYDirection].x
-  //   ) {
-  //     x = BOARD_SQUARE_ARR[i].x;
-  //   }
-  //   if (mouseY > BOARD_SQUARE_ARR[i].y && mouseY < BOARD_SQUARE_ARR[i + 1].y) {
-  //     y = BOARD_SQUARE_ARR[i].y;
-  //   }
-
-  //   fill(180, 28, 28);
-  //   // square(x, y, BOARD_SQUARE_SIZE);
-  //   if (x != null && y != null) {
-  //     square(x, y, BOARD_SQUARE_SIZE);
-  //     const squareCoordinate = { x, y, filled, alive };
-
-  //     SQUARE_ARR.push(squareCoordinate);
-  //     // console.log(SQUARE_ARR);
-
-  //     return;
-  //   }
-  // }
 }
 
-// class Square {
-
-//     // constructor (x, y, alive){
-//     //   this.x = x
-//     //   this.y = y
-//     //   this.alive = alive
-//     // };
-
-//     static starterSquare() {
-//       let randomStart = int(random(BOARD_SQUARE_ARR.length))
-//       BOARD_SQUARE_ARR[randomStart].alive ? fill(180, 28, 28) : fill(0);
-//       square(BOARD_SQUARE_ARR[randomStart].x, BOARD_SQUARE_ARR[randomStart].y, BOARD_SQUARE_SIZE);
+// function checkNeighbourAlive(){
+//   let countAliveNeighbour = 0;
+//   for (let x = 0; x < BOARD_SQUARE_ARR_2D.length; x++) {
+//     for (let y = 0; y < BOARD_SQUARE_ARR_2D[x].length; y++) {
+//       if(BOARD_SQUARE_ARR_2D[x][y].alive === true){
+//         if(BOARD_SQUARE_ARR_2D[x-BOARD_SQUARE_SIZE-1][y-BOARD_SQUARE_SIZE-1].alive === true){
+//           countAliveNeighbour ++
+//         }
+//         if(BOARD_SQUARE_ARR_2D[x][y-BOARD_SQUARE_SIZE-1].alive === true){
+//           countAliveNeighbour ++
+//         }
+//         if(BOARD_SQUARE_ARR_2D[x+BOARD_SQUARE_SIZE+1][y-BOARD_SQUARE_SIZE-1].alive === true){
+//           countAliveNeighbour ++
+//         }
+//         if(BOARD_SQUARE_ARR_2D[x-BOARD_SQUARE_SIZE-1][y].alive === true){
+//           countAliveNeighbour ++
+//         }
+//         if(BOARD_SQUARE_ARR_2D[x+BOARD_SQUARE_SIZE+1][y].alive === true){
+//           countAliveNeighbour ++
+//         }
+//         if(BOARD_SQUARE_ARR_2D[x-BOARD_SQUARE_SIZE-1][y+BOARD_SQUARE_SIZE+1].alive === true){
+//           countAliveNeighbour ++
+//         }
+//         if(BOARD_SQUARE_ARR_2D[x][y+BOARD_SQUARE_SIZE+1].alive === true){
+//           countAliveNeighbour ++
+//         }
+//         if(BOARD_SQUARE_ARR_2D[x+BOARD_SQUARE_SIZE+1][y+BOARD_SQUARE_SIZE+1].alive === true){
+//           countAliveNeighbour ++
+//         }
+//         //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+//         if(countAliveNeighbour<2){
+//           BOARD_SQUARE_ARR_2D[x][y].alive = false
+//         }
+//         //Any live cell with two or three live neighbours lives on to the next generation.
+//         if(countAliveNeighbour===2 || countAliveNeighbour===3){
+//           BOARD_SQUARE_ARR_2D[x][y].alive = true
+//         }
+//         //Any live cell with more than three live neighbours dies, as if by overpopulation.
+//         if(countAliveNeighbour > 3){
+//           BOARD_SQUARE_ARR_2D[x][y].alive = false
+//         }
+//         //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+//         if(countAliveNeighbour === 3){
+//           BOARD_SQUARE_ARR_2D[x][y].alive = true
+//         }
+//       }
+//       console.log(countAliveNeighbour)
+//       countAliveNeighbour = 0;
 //     }
+//   }
+// }
 
-//     checkNeighbour() {
-//       starterSquare().Square
-//     }
+function checkNeighbourAlive(){
+  let countAliveNeighbour = 0;
+  for (let x = 0; x < BOARD_SQUARE_ARR_2D.length; x++) {
+    for (let y = 0; y < BOARD_SQUARE_ARR_2D[x].length; y++) {
+      if(BOARD_SQUARE_ARR_2D[x][y].alive === true){
+        if(BOARD_SQUARE_ARR_2D[x-1][y-1].alive === true){
+          countAliveNeighbour ++
+        }
+        if(BOARD_SQUARE_ARR_2D[x][y-1].alive === true){
+          countAliveNeighbour ++
+        }
+        if(BOARD_SQUARE_ARR_2D[x+1][y-1].alive === true){
+          countAliveNeighbour ++
+        }
+        if(BOARD_SQUARE_ARR_2D[x-1][y].alive === true){
+          countAliveNeighbour ++
+        }
+        if(BOARD_SQUARE_ARR_2D[x+1][y].alive === true){
+          countAliveNeighbour ++
+        }
+        if(BOARD_SQUARE_ARR_2D[x-1][y+1].alive === true){
+          countAliveNeighbour ++
+        }
+        if(BOARD_SQUARE_ARR_2D[x][y+1].alive === true){
+          countAliveNeighbour ++
+        }
+        if(BOARD_SQUARE_ARR_2D[x+1][y+1].alive === true){
+          countAliveNeighbour ++
+        }
+        //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        if(countAliveNeighbour<2){
+          BOARD_SQUARE_ARR_2D[x][y].alive = false
+        }
+        //Any live cell with two or three live neighbours lives on to the next generation.
+        if(countAliveNeighbour===2 || countAliveNeighbour===3){
+          BOARD_SQUARE_ARR_2D[x][y].alive = true
+        }
+        //Any live cell with more than three live neighbours dies, as if by overpopulation.
+        if(countAliveNeighbour > 3){
+          BOARD_SQUARE_ARR_2D[x][y].alive = false
+        }
+        //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+        if(countAliveNeighbour === 3){
+          BOARD_SQUARE_ARR_2D[x][y].alive = true
+        }
+      }
+      console.log(countAliveNeighbour)
+      countAliveNeighbour = 0;
+    }
+  }
+}
 
-//}
+function arrayAssingment(){
+  for (let x = 0; x < BOARD_SQUARE_ARR_2D.length; x++) {
+    for (let y = 0; y < BOARD_SQUARE_ARR_2D[x].length; y++) {
+      BOARD_SQUARE_ARR_2D[x][y] = SQUARE_ARR_2D[x][y];
+    }
+  }
+}
+
+function cellPaint(){
+  for (let x = 0; x < BOARD_SQUARE_ARR_2D.length; x++) {
+    for (let y = 0; y < BOARD_SQUARE_ARR_2D[x].length; y++) {
+      if(BOARD_SQUARE_ARR_2D[x][y].alive)
+      fill(180, 28, 28);
+      square(BOARD_SQUARE_ARR_2D[x][y].x, BOARD_SQUARE_ARR_2D[x][y].y, BOARD_SQUARE_SIZE);
+    }
+  }
+}
