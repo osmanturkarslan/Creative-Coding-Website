9e4458e4d143af7611f2ccd1ac7f4471d4c3e609
osmanturkarslan <osmanturkarslan1620@gmail.com>
2024-06-06 20:57:23 +0200

    some fixes

--- Javascript/markov_chain.js
+++ Javascript/markov_chain.js
@@ -11,12 +11,18 @@ function preload() {
 }
 
 function setup() {
+  createCanvas(windowWidth, windowHeight)
   background(200);
-
+  frameRate(10);
   generateWordArray();
   uniqueWordMapCreator();
-  generateSentences(200);
+  generateSentences(1000);
   console.log(generatedSentence);
+  textAlign(CENTER);
+  textSize(30)
+  textWrap(WORD);
+  textStyle(ITALIC);
+  text(generatedSentence, 0,50,width,height-150)
 }
 
 function generateWordArray() {
@@ -44,16 +50,9 @@ function uniqueWordMapCreator() {
 function generateSentences(numberOfWords = 10) {
   current = random(Array.from(wordMap.keys()));
   for (let i = 0; i < numberOfWords; i++) {
-    if(!isSpecialCharacter(current)){
-        generatedSentence += " "
-    }
+    generatedSentence += " "
     generatedSentence += current
     let array = wordMap.get(current);
     current = random(array);
   }
-}
-
-function isSpecialCharacter(str){
-    //TODO
-    return false
 }
